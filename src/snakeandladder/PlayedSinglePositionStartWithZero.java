9c57f3b1c7557fee0fb4f3ed7ea3404a8b57bc31
dhiru0904 <dhirajdevre4@gmail.com>
2022-11-25 11:52:19 +0530

    Added: snake and ladder start with zero position

---
+++ src/snakeandladder/PlayedSinglePositionStartWithZero.java
@@ -0,0 +1,17 @@
+package snakeandladder;
+
+public class PlayedSinglePositionStartWithZero {
+    PlayedSinglePositionStartWithZero() {
+
+        System.out.println("Welcome to Snake and Ladder Problem");
+    }public static void main(String args[]){
+        PlayedSinglePositionStartWithZero input = new PlayedSinglePositionStartWithZero();
+
+        /*
+        UC-1 Snake & Ladder game played with single played at start position.
+         */
+        int start_position=0;
+        System.out.println("Single player at starting position "+start_position);
+    }
+}
+
